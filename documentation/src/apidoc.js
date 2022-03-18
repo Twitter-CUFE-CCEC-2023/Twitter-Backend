f6ff11d14b7f54f2ec8c1d51a6882b72656d1f5c
amrzakii2000 <amrz@silverkeytech.com>
2022-03-18 16:41:55 +0200

    Added first api

---
+++ documentation/src/apidoc.js
@@ -0,0 +1,13 @@
+/**
+ * @api {post} /auth/login/ Login user into the website
+ * @apiVersion 0.1.0
+ * @apiName LoginUser
+ * @apiGroup Authentication
+ * @apiDescription Generates authentication token for the user to login into the website. User can login usiing (email or username) and password
+ * @apiSampleRequest off
+ * @apiParam {String} email or username Email or Username of the user
+ * @apiParam {String} password Password of the user
+
+
+
+**/
