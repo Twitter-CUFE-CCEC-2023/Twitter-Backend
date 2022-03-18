bcce4fed95387d8d685596d8ece4a43e2632058c
AbdelrhmanAmrAli <76621761+AbdelrhmanAmrAli@users.noreply.github.com>
2022-03-18 21:52:08 +0200

    Added Unblock user

--- documentation/src/apidoc.js
+++ documentation/src/apidoc.js
@@ -242,3 +242,36 @@
  *       message: "Invalid user credentials"
  * }
  **/
+
+
+/**
+ * @api {POST} /user/unblock Unblock user
+ * @apiVersion 0.1.0
+ * @apiName Unblock user
+ * @apiGroup User
+ * @apiDescription Un-blocks the user specified in the ID parameter for the authenticating user. Returns the un-blocked user when successful. If relationships existed before the block was instantiated, they will not be restored.
+ * @apiSampleRequest off 
+ * @apiPermission RequiresAuth
+ * @apiParam {String} access_token JWT generated access token for the user. It is sent in header
+ * @apiParam {int} [user_id] The ID of the  blocked user
+ * @apiParamExample {json} Request-Example: 
+ * {
+ *      "user_id": 20,
+ * }
+ * @apiSuccess {String} message Success message
+ * @apiSuccessExample {json} Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ *      "message": "User Unblocked successfully"
+ * }
+ * @apiError (400) {String} BadRequest  The server cannot or will not process the request due to something that is perceived to be a client error
+ * @apiError (500) {String} InternalServerError  The server encountered an unexpected condition which prevented it from fulfilling the request
+ * @apiError (401) {String} Unauthorized  User is not authenticated
+ * @apiError (404) {String} UserNotFound  invalid user_id
+
+ * @apiErrorExample {json} Error-Response:
+ * HTTP/1.1 404 UserNotFound
+ * {
+ *       message: "Invalid user credentials"
+ * }
+ **/
