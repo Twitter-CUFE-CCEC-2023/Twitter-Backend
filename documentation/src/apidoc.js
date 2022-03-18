9dc3c2dfadbd944f9bf9f7ea0d0ab1e586823371
Ammar Yasser <52798819+ammaryassereng@users.noreply.github.com>
2022-03-18 20:29:25 +0200

    Tweet_get_by_id_finally_Done

--- documentation/src/apidoc.js
+++ documentation/src/apidoc.js
@@ -1,4 +1,8 @@
 /**
+ * @apiDefine RequiresAuth Authentication is required
+ **/
+
+/**
  * @api {post} /auth/login Login user into the website
  * @apiVersion 0.1.0
  * @apiName LoginUser
@@ -36,7 +40,8 @@
  * @apiGroup Tweets
  * @apiDescription Returns a single Tweet, specified by the id parameter. The Tweet's author will also be embedded within the Tweet. 
  * @apiSampleRequest off
- * @apiParam {Number} id id of the tweet
+ * @apiPermission RequiresAuth
+ * @apiParam {int} id id of the tweet
  * @apiParam {Booleans} [include_my_retweet] When set to either true , t or 1 , any Tweets returned that have been retweeted by the authenticating user will include an additional current_user_retweet node, containing the ID of the source status for the retweet.
  * @apiParamExample {json} Request-Example: 
  * {
@@ -48,7 +53,10 @@
  * {
  *      "created_at": "Wed Oct 10 20:19:24 +0000 2018",
  *      "id": 1050118621198921728,
- *      "text": "To make room for more expression, we will now count all emojis as equal—including those with gender‍‍‍ and skin t… https://t.co/MkGjXf9aXm"
+ *      "text": "To make room for more expression, we will now count all emojis as equal—including those with gender‍‍‍ and skin t… https://t.co/MkGjXf9aXm",
+ *      "user_mentions": [],
+ *      "urls": [],
+ *      "user": userObj
  * }
  * @apiError (400) {String} BadRequest  The server cannot or will not process the request due to something that is perceived to be a client error
  * @apiError (500) {String} InternalServerError  The server encountered an unexpected condition which prevented it from fulfilling the request
