f6d9a509fe777e2717292ce7ee948856a158395e
Ammar Yasser <52798819+ammaryassereng@users.noreply.github.com>
2022-03-18 20:07:01 +0200

    TweetGetByID

--- documentation/src/apidoc.js
+++ documentation/src/apidoc.js
@@ -32,12 +32,11 @@
 /**
  * @api {get} status/tweet/RetriveTweet retrive a tweet by id
  * @apiVersion 0.1.0 
- * @apiNamw getTweet
+ * @apiName getTweet
  * @apiGroup Tweets
  * @apiDescription Returns a single Tweet, specified by the id parameter. The Tweet's author will also be embedded within the Tweet. 
  * @apiSampleRequest off
  * @apiParam {Number} id id of the tweet
- * @apiParam {Booleans} [trim_user] When set to either true , t or 1 , each Tweet returned in a timeline will include a user object including only the status authors numerical ID. Omit this parameter to receive the complete user object.
  * @apiParam {Booleans} [include_my_retweet] When set to either true , t or 1 , any Tweets returned that have been retweeted by the authenticating user will include an additional current_user_retweet node, containing the ID of the source status for the retweet.
  * @apiParamExample {json} Request-Example: 
  * {
@@ -47,14 +46,18 @@
  * @apiSuccessExample {json} Success-Response:
  * HTTP/1.1 200 OK
  * {
- *      "access_token": 1289654
+ *      "created_at": "Wed Oct 10 20:19:24 +0000 2018",
+ *      "id": 1050118621198921728,
+ *      "text": "To make room for more expression, we will now count all emojis as equal—including those with gender‍‍‍ and skin t… https://t.co/MkGjXf9aXm"
  * }
  * @apiError (400) {String} BadRequest  The server cannot or will not process the request due to something that is perceived to be a client error
  * @apiError (500) {String} InternalServerError  The server encountered an unexpected condition which prevented it from fulfilling the request
- * @apiError (401) {String} UserNotFound  The enetered credentials are invalid
+ * @apiError (401) {String} UnAuthorized  user is not authenticated 
+ * @apiError (404) {String} TweetNotFound  The enetered credentials are invalid
  * @apiErrorExample {json} Error-Response:
  * HTTP/1.1 401 Unauthorized
  * {
  *       message: "tweet not found"
  * }
 **/
+
