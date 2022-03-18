06a17a287af16ce76effd7b78c8162a114742364
amrzakii2000 <amrz@silverkeytech.com>
2022-03-18 17:22:49 +0200

    Added login request

--- documentation/src/apidoc.js
+++ documentation/src/apidoc.js
@@ -1,13 +1,30 @@
 /**
- * @api {post} /auth/login/ Login user into the website
+ * @api {post} /auth/login Login user into the website
  * @apiVersion 0.1.0
  * @apiName LoginUser
  * @apiGroup Authentication
  * @apiDescription Generates authentication token for the user to login into the website. User can login usiing (email or username) and password
  * @apiSampleRequest off
- * @apiParam {String} email or username Email or Username of the user
+ * @apiParam {String} email_or_username Email or Username of the user
  * @apiParam {String} password Password of the user
-
-
+ * @apiParamExample {json} Request-Example: 
+ * {
+ *      "email_or_username": "amrzaki2000.az@gmail.com",
+ *      "password": "myPassw@ord123"
+ * }
+ * @apiSuccess {String} access_token JWT generated access token for the user
+ * @apiSuccessExample {json} Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ *      "access_token": "AAAA%2FAAA%3DAAAAAAAAxxxxxx"
+ * }
+ * @apiError (400) {String} BadRequest  The server cannot or will not process the request due to something that is perceived to be a client error
+ * @apiError (500) {String} InternalServerError  The server encountered an unexpected condition which prevented it from fulfilling the request
+ * @apiError (401) {String} UserNotFound  The enetered credentials are invalid
+ * @apiErrorExample {json} Error-Response:
+ * HTTP/1.1 401 Unauthorized
+ * {
+ *       message: "User not found"
+ * }
 
 **/
