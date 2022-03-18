39b688491058a384c02e118fba8dad20eb7d8cc3
Ammar Yasser <52798819+ammaryassereng@users.noreply.github.com>
2022-03-18 18:25:19 +0200

    Added_Retrive_Tweet

--- documentation/src/apidoc.js
+++ documentation/src/apidoc.js
@@ -28,3 +28,33 @@
  * }
 
 **/
+
+/**
+ * @api {get} status/tweet/RetriveTweet retrive a tweet by id
+ * @apiVersion 0.1.0 
+ * @apiNamw getTweet
+ * @apiGroup Tweets
+ * @apiDescription Returns a single Tweet, specified by the id parameter. The Tweet's author will also be embedded within the Tweet. 
+ * @apiSampleRequest off
+ * @apiParam {Number} id id of the tweet
+ * @apiParam {Booleans} [trim_user] When set to either true , t or 1 , each Tweet returned in a timeline will include a user object including only the status authors numerical ID. Omit this parameter to receive the complete user object.
+ * @apiParam {Booleans} [include_my_retweet] When set to either true , t or 1 , any Tweets returned that have been retweeted by the authenticating user will include an additional current_user_retweet node, containing the ID of the source status for the retweet.
+ * @apiParamExample {json} Request-Example: 
+ * {
+ *      "id": 123456
+ * }
+ * @apiSuccess {Number} access_token JWT generated access token for the user
+ * @apiSuccessExample {json} Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ *      "access_token": 1289654
+ * }
+ * @apiError (400) {String} BadRequest  The server cannot or will not process the request due to something that is perceived to be a client error
+ * @apiError (500) {String} InternalServerError  The server encountered an unexpected condition which prevented it from fulfilling the request
+ * @apiError (401) {String} UserNotFound  The enetered credentials are invalid
+ * @apiErrorExample {json} Error-Response:
+ * HTTP/1.1 401 Unauthorized
+ * {
+ *       message: "tweet not found"
+ * }
+**/
