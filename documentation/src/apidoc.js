4912dab46d8c5687b529add98efb8335b5e7d93b
Ammar Yasser <52798819+ammaryassereng@users.noreply.github.com>
2022-03-19 02:14:09 +0200

    Adding access token to sign up

--- documentation/src/apidoc.js
+++ documentation/src/apidoc.js
@@ -668,27 +668,33 @@
  * @apiParam {String} email Email of the user
  * @apiParam {String} username Username of the user
  * @apiParam {String} password password of the user
- * @apiParam {String} first_Name first name of the user
- * @apiParam {String} second_Name second name of the user
+ * @apiParam {String} first_name first name of the user
+ * @apiParam {String} second_name second name of the user
  * @apiParam {String} gender gender of the user
- * @apiParam {DateTime} BirthDate birth date of the user
- * @apiParam {int} phoneNumber phone number of the user
+ * @apiParam {DateTime} birth_date birth date of the user
+ * @apiParam {int} phone_number phone number of the user
  * @apiParamExample {json} Request-Example:
  * {
  *      "email": "amrzaki2000.az@gmail.com",
  *      "username": "amrZaki123"
  *      "password": "myPassw@ord123",
- *      "first_Name": "Amr",
- *      "second_Name": "Zaki",
+ *      "first_name": "Amr",
+ *      "second_name": "Zaki",
  *      "gender": "male",
- *      "BirthDate": "2000-01-01T00:00:00.000Z",
- *      "phoneNumber": "0105267436"
+ *      "birth_date": "2000-01-01T00:00:00.000Z",
+ *      "phone_number": "0105267436"
  * }
  * @apiSuccess {String} message Success message
+ * @apiSuccess {String} access_token JWT generated access token for the user
+ * @apiSuccess {user-object} user of the sign up operation
+ * @apiSuccess {DateTime} token_expiration_date The date and time of token expiration
  * @apiSuccessExample {json} Success-Response:
  * HTTP/1.1 200 OK
  * {
+ *      "access_token": "AAAA%2FAAA%3DAAAAAAAAxxxxxx",
+ *      "user" : {user-object},
  *      "role": "user",
+ *      "token_expiration_date": "2020-01-01T00:00:00.000Z",
  *      "message": "User Signed up successfully"
  * }
  * @apiError (400) {String} BadRequest  The server cannot or will not process the request due to something that is perceived to be a client error
