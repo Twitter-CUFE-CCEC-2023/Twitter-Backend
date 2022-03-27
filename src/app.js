0c42da2505a9ed00677e948e480b8334919c8d72
amrzakii2000 <amrz@silverkeytech.com>
2022-03-27 21:56:30 +0200

    Added routes to app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/user");
+const tweetRoutes = require("./routes/tweet");
+const timelineRoutes = require("./routes/timeline");
+
 const app = express();
 const port = process.env.Port || 3000;
 
 const connectionurl = "mongodb://localhost:27017/twitter-clone";
 
 app.use(express.json());
+app.use(adminRoutes);
+app.use(userRoutes);
+app.use(tweetRoutes);
+app.use(timelineRoutes);
 
 app.listen(port, () => {
   mongoose.connect(
