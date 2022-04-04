63ef29608fae7d1028ff7f2466ba55d68e5eac46
amrzakii2000 <amrz@silverkeytech.com>
2022-04-04 03:57:48 +0200

    Added user data seed

---
+++ seed-data/user.js
@@ -0,0 +1,26 @@
+const userRole = require('./constants/userRole');
+
+const users = [
+  {
+    _id: "624a4a94c66738f13854b474",
+    name: "Amr Zaki",
+    username: "amrzaki",
+    email: "amrzaki2000.az@gmail.com",
+    password: "12345678",
+    dateOfBirth: "2000-10-17",
+    roleId: userRole.defaultRole._id,
+    isVerified: true
+  },
+  {
+    _id: "624a4fbf3f392aefdb4dd1c8",
+    name: "Ahmed Ibrahim",
+    username: "ahmedElgarf",
+    email: "ahmedelgarf@gmail.com",
+    password: "12345678",
+    dateOfBirth: "1999-4-10",
+    roleId: userRole.adminRole._id,
+    isVerified: true
+  },
+];
+
+module.exports = accessTypes;
