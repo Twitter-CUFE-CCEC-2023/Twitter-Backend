9d741aa94710988b179a40a89ef812c8dec9503b
amrzakii2000 <amrz@silverkeytech.com>
2022-04-04 02:18:05 +0200

    Added constants seed data

---
+++ seeders/birth-information-access.seeder.js
@@ -0,0 +1,19 @@
+const Seeder = require("mongoose-data-seed").Seeder;
+const birthInformationModel = require("./../src/models/constants/birthInformationAccess");
+
+const data = require("./../seed-data/constants/birthInformationAccess");
+
+class BirthInformationAccessSeeder extends Seeder {
+  async shouldRun() {
+    return birthInformationModel
+      .countDocuments()
+      .exec()
+      .then((count) => count === 0);
+  }
+
+  async run() {
+    return birthInformationModel.create(data);
+  }
+}
+
+module.exports = BirthInformationAccessSeeder;
