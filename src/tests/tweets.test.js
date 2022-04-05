8ef4fc69f3532cfe6b6327e16aad79b71ddefc73
Ammar Yasser <52798819+ammaryassereng@users.noreply.github.com>
2022-04-06 01:14:54 +0200

    Testing tweets done

--- src/tests/tweets.test.js
+++ src/tests/tweets.test.js
@@ -5,17 +5,42 @@ const config = require("../config");
 const Tweets = require("../models/tweet");
 const User = require("../models/user");
 
+const connectionurl = config.testConnectionSting;
+
+beforeAll(() => {
+    mongoose.connect(
+        connectionurl,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        (error, result) => {
+            if (error) {
+                throw error;
+            }
+        }
+    );
+});
+
+
+afterAll(() => {
+    mongoose.connection.close();
+});
+
+
 id = new mongoose.Types.ObjectId()
 
 const tweet1 = {
     _id: id,
-    username: "zikaaaaaa"
+    username: "zikaaaaaa",
+    content: "I am the first tweet"
 }
 
 const tweet2 = {
     username: "zikaaaaaa",
     parentId: id,
-    isRetweeted: true
+    isRetweeted: true,
+    content: "I am the second tweet"
 }
 
 const userOne = {
@@ -27,9 +52,19 @@ const userOne = {
 }
 
 beforeEach(async () => {
-    await new User(userOne).save()
+    await User.deleteMany({});
+    await new User(userOne).save();
+    await Tweets.deleteMany({});
     await new Tweets(tweet1).save()
     await new Tweets(tweet2).save()
+});
+
+test('getting a tweet and the writing user from the tweet id', async()=>{
+    await request(app).get('/status/tweet/' + id).send({}).expect(200)
+})
+
+test('getting the tweets by the username', async()=> {
+    await request(app).get('/status/tweets/list/' + userOne.username).send({}).expect(200)
 })
 
 test('deleting a tweet', async()=>{
