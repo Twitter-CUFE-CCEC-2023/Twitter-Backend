c02e1af8accdeb1dc0da87780290d053de913146
amrzakii2000 <amrz@silverkeytech.com>
2022-04-06 01:06:41 +0200

    Added home timeline and notifications

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const notificationModel = require("./../models/notification.js");
+const tweetModel = require("./../models/tweet");
+const userModel = require("./../models/user.js");
 require("./../models/constants/notificationType.js");
-require("./../models/tweet.js");
-require("./../models/user.js");
 
 router.get("/notifications/list", async (req, res) => {
   try {
     const id = req.body.userId;
+    const user = await userModel.findOne({ _id: id }).select("username -_id");
+    const username = user["username"];
     const count = 2;
+
     if (isNaN(req.body.page) && req.body.page != "" && req.body.page != null) {
       return res.status(400).send({ message: "Invalid page number" });
     }
@@ -26,16 +29,26 @@ router.get("/notifications/list", async (req, res) => {
       .populate({
         path: "notificationTypeId",
         select: "name -_id",
+      })
+      .populate({
+        path: "tweetId",
       });
 
     if (!result) {
       res.status(404).send({ error_message: "Notifications not found" });
     }
 
+    // for (let i = 0; i < result.length; i++) {
+    //   const tweet = result[i]["tweetId"];
+    //   tweetModel.getTweetInfobyId(tweet["_id"], username).then((tweetInfo) => {
+    //     result[i]["tweetInfo"] = tweetInfo;
+    //   });
+    // }
+
     res.status(200).send(result);
   } catch (err) {
     //res.status(500).send({ message: "Error in getting notifications" });
-    res.status(500).send(err.toString());
+    res.status(500).send(err);
   }
 });
 
