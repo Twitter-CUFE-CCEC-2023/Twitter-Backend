143ed3e71008b3dc73e4b6271b6c1202d5ed0362
ahmedelgarf94 <ahmedelgarf94@gmail.com>
2022-04-05 19:42:20 +0200

    Update CloudConnectionString

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 exports.devConnectionSting = "mongodb://localhost:27017/twitter-clone";
 exports.cloudConnectString =
-  "mongodb+srv://amrzaki:5aropJWrQ5s5vs3h@software-project.cr4po.mongodb.net/twitter?retryWrites=true&w=majority";
+  "mongodb+srv://ahmedelgarf:CDB88jw1G5OaTnUY@software-project.cr4po.mongodb.net/twitter?retryWrites=true&w=majority";
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,10 @@
 const express = require("express");
-
+const User = require("../models/user");
+const fellowUser = require("../models/followUser");
 const router = express.Router();
+
+
+
+
 
 module.exports = router;
