d40123978ccb71794ef77352cdb98ae5ba122f05
ahmedelgarf94 <ahmedelgarf94@gmail.com>
2022-04-05 19:52:00 +0200

    Added following list

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,10 +6,24 @@ const router = express.Router();
 
 router.get('/followers/list/:username', async (req, res) => {
     const _username = req.params.username
-    console.log(_username)
     try {
         const users = await fellowUser.find({
             followingUsername:  _username
+        })
+        if (!users) {
+            return res.status(404).send()
+        }
+        res.send(users)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.get('/following/list/:username', async (req, res) => {
+    const _username = req.params.username
+    try {
+        const users = await fellowUser.find({
+            username:  _username
         })
         if (!users) {
             return res.status(404).send()
@@ -20,6 +34,7 @@ router.get('/followers/list/:username', async (req, res) => {
         res.status(500).send()
     }
 })
+
 
 
 
