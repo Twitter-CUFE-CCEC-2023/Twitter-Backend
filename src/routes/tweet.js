fae8fbac968870800b8daca663165a3b69ceb6b6
Ammar Yasser <52798819+ammaryassereng@users.noreply.github.com>
2022-04-05 21:13:29 +0200

    finish the retrive tweet by id

--- src/routes/tweet.js
+++ src/routes/tweet.js
@@ -25,8 +25,8 @@ router.delete('/status/tweet/delete', async (req, res)=>{
 
 router.get('/status/tweets/list/:username', async (req, res)=>{
     try{
-        console.log(req.params.username)
-        console.log(req.query.include_replies)
+        //console.log(req.params.username)
+        //console.log(req.query.include_replies)
         tweets = undefined
         if(req.query.include_replies == "true")
         {
@@ -49,5 +49,33 @@ router.get('/status/tweets/list/:username', async (req, res)=>{
     }
 })
 
+router.get('/status/tweet/:id', async(req, res)=>{
+    try{
+        tweet = undefined
+        if(req.query.include_my_retweet	== "true")
+        {
+            tweet = await Tweet.findById(req.params.id)
+        }
+        else
+        {
+            tweet = await Tweet.findOne({_id:req.params.id,isRetweeted:false})
+        }
+        if(!tweet){
+            return res.status(404).send()
+        }
+        user = await User.findOne({username: tweet.username})  
+        if(!user){
+            return res.status(404).send()
+        }
+        res.status(200).send({
+            tweet: tweet,
+            user: user,
+            message: 'tweets and user retrieved successfully'
+        })
+    }
+    catch{
+        res.status(500).send()
+    }
+})
 
 module.exports = router;
