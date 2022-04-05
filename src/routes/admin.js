77578907306c2314d379f0a73cace9201af77a0b
AbdelrhmanAmrAli <76621761+AbdelrhmanAmrAli@users.noreply.github.com>
2022-04-05 21:23:19 +0200

    Added unban user

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -10,8 +10,6 @@ router.post('/dashboard/ban', async (req, res) => {
     const allowedUpdates = ['userId','isBanned','banDuration','reason','isPermanent']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
     if (!isValidOperation) {
-        console.log(req.body.userId)
-        console.log(req.body.isBanned)
         return res.status(400).send({ error: 'Invalid updates!' })
     }
     try {
@@ -25,6 +23,33 @@ router.post('/dashboard/ban', async (req, res) => {
         res.status(200).send({
             user:user,
             message:'User Banned successfully'
+        })
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+
+
+
+router.post('/dashboard/unban', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['userId','isBanned']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.body.userId, {isBanned:false}, { new: true, runValidators: true })
+        const banuser = await banUser.deleteOne({userId:req.body.userId})
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.status(200).send({
+            user:user,
+            message:'User unBanned successfully'
         })
     } catch (e) {
         res.status(500).send(e)
