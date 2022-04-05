cafe72285925ef1c596969481c8daab070e263be
Ammar Yasser <52798819+ammaryassereng@users.noreply.github.com>
2022-04-05 17:30:19 +0200

    delete a tweet done

--- src/routes/tweet.js
+++ src/routes/tweet.js
@@ -1,5 +1,35 @@
 const express = require("express");
+const Tweet = require('../models/tweet')
+const User = require("../models/user");
 
 const router = express.Router();
+
+router.delete('/status/tweet/delete', async (req, res)=>{
+    try{
+        const tweet = await Tweet.findByIdAndDelete(req.body.id)
+
+        if(!tweet){
+            return res.send(404).send()
+        }
+        res.status(200).send({
+            tweet: tweet,
+            message: 'tweet deleted successfully'
+        })
+        
+    }
+    catch{
+        res.status(500).send()
+    }
+})
+
+router.get('/status/tweets/list/:username?include_replies=true', async (req, res)=>{
+    try{
+
+    }
+    catch{
+        res.status(500).send()
+    }
+})
+
 
 module.exports = router;
