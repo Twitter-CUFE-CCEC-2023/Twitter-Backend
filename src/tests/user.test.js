5ee0fdd7e8f7ee3c72bee1fa51833befa00b66b5
ahmedelgarf94 <ahmedelgarf94@gmail.com>
2022-04-06 00:34:09 +0200

    Added test function to test get following list

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -4,35 +4,70 @@ const app = require("./testApp");
 const mongoose = require("mongoose");
 const config = require("../config");
 const User = require("../models/user");
-
+const followUserModel = require("../models/followUser");
 const connectionurl = config.testConnectionSting;
 
+const userOneId= new mongoose.Types.ObjectId();
+const userTwoId= new mongoose.Types.ObjectId();
+
+const userOne = {
+    _id: userOneId,
+    name:"Amr Zaki",
+    username: "zika",
+    dateOfBirth: "2000-01-01T00:00:00.000Z",
+    email: "zika@gmail.com",
+    password: "myPassw@ord123",
+}
+
+const userTwo = {
+    _id: userTwoId,
+    name:"Ahmed Elgarf",
+    username: "elgarf",
+    dateOfBirth: "1999-10-10T00:00:00.000Z",
+    email: "elgarf@gmail.com",
+    password: "TTFTTSTTD",
+}
+
+const followUserOne={
+    userId: userOneId,
+    followingUserId: userTwoId,
+}
+const followUserTwo={
+    userId: userTwoId,
+    followingUserId: userOneId,
+}
+beforeEach(async () => {
+    await User.deleteMany({});
+    await new User(userOne).save();
+    await new User(userTwo).save();
+    await new followUserModel(followUserOne).save();
+    await new followUserModel(followUserTwo).save();
+  });
 
 beforeAll(() => {
     mongoose.connect(
-      connectionurl,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-      (error, result) => {
-        if (error) {
-          throw error;
+        connectionurl,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        (error, result) => {
+            if (error) {
+                throw error;
+            }
         }
-      }
     );
-  });
+});
 
 
-  afterAll(() => {
+afterAll(() => {
     mongoose.connection.close();
-  });
+});
 
 
-  test("Should get following list", async () => {
+test("Should get following list", async () => {
     const response = await request(app)
-      .get("/following/list/:amrzaki")
-      .send()
-      .expect(200);
-  });
-  
+        .get("/following/list/:zika")
+        .send()
+        .expect(200);
+});
