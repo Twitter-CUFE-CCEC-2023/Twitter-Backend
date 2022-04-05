ce4cd9a4e0c5e9d4361d118aa1c937372eb2d2f4
amrzakii2000 <amrz@silverkeytech.com>
2022-04-06 00:27:17 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 exports.devConnectionSting = "mongodb://localhost:27017/twitter-clone";
 exports.cloudConnectString =
+  "mongodb+srv://ahmedelgarf:CDB88jw1G5OaTnUY@software-project.cr4po.mongodb.net/twitter?retryWrites=true&w=majority";
 exports.testConnectionSting = "mongodb://localhost:27017/twitter-clone-testDB";
 exports.verificationEmail = "";
 exports.verificationPassword = "";
