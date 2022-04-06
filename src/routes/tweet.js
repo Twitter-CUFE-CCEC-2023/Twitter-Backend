383a6e06d6b3c3eb688c5ddde6e4e8e1a6fabc3f
Ammar Yasser <52798819+ammaryassereng@users.noreply.github.com>
2022-04-06 04:25:06 +0200

    Finished Tweets

--- src/routes/tweet.js
+++ src/routes/tweet.js
@@ -4,11 +4,11 @@ const User = require("../models/user");
 
 const router = express.Router();
 
-router.delete('/status/tweet/delete', async (req, res)=>{
-    try{
+router.delete('/status/tweet/delete', async (req, res) => {
+    try {
         const tweet = await Tweet.findByIdAndDelete(req.body.id)
 
-        if(!tweet){
+        if (!tweet) {
             return res.status(404).send()
         }
 
@@ -16,57 +16,79 @@ router.delete('/status/tweet/delete', async (req, res)=>{
             tweet: tweet,
             message: 'tweet deleted successfully'
         })
-        
+
     }
-    catch{
+    catch {
         res.status(500).send()
     }
 })
 
-router.get('/status/tweets/list/:username', async (req, res)=>{
-    try{
+router.get('/status/tweets/list/:username', async (req, res) => {
+    try {
         tweets = undefined
-        if(req.query.include_replies == "true")
-        {
-            tweets = await Tweet.find({ username: req.params.username}).sort({ createdAt: -1 })  
+        if (req.query.include_replies == "true") {
+            tweets = await Tweet.find({ username: req.params.username }).sort({ createdAt: -1 })
         }
-        else
-        {
-            tweets = await Tweet.find({ username: req.params.username, parentId: null}).sort({ createdAt: -1 })
+        else {
+            tweets = await Tweet.find({ username: req.params.username, parentId: null }).sort({ createdAt: -1 })
         }
-        if(!tweets){
-            return res.status(404).send()
+        if (!tweets) {
+            return res.status(400).send()
         }
-        res.status(200).send({
+
+        const getTweets = tweets.map(async (item) => {
+            const tweetInfo = await Tweet.getTweetInfobyId(
+                item._id,
+                req.params.username
+            );
+            if (tweetInfo.error) {
+                return item;
+            }
+            item.tweetInfo = tweetInfo;
+            return item;
+        });
+
+        Promise.all(getTweets)
+            .then((tweets) => {
+                res.status(200).send(tweets);
+            })
+            .catch((error) => {
+                throw error;
+            });
+
+
+        /*res.status(200).send({
             tweets: tweets,
             message: 'tweets retrieved successfully'
-        })
+        })*/
     }
-    catch{
+    catch {
         res.status(500).send()
     }
 })
 
-router.get('/status/tweet/:id', async(req, res)=>{
-    try{
+router.get('/status/tweet/:id', async (req, res) => {
+    try {
         tweet = undefined
-        tweet = await Tweet.findById(req.params.id).populate({path:"userId",select:"username name profile_picture -_id"})
+        tweet = await Tweet.findById(req.params.id).populate({ path: "userId", select: "username name profile_picture -_id" })
 
-        if(!tweet){
+        if (!tweet) {
             return res.status(400).send("invalid input")
         }
-        const answer = await Tweet.getTweetInfobyId(tweet._id,tweet.username)
-        
-        if(answer.error){
+        const answer = await Tweet.getTweetInfobyId(tweet._id, tweet.username)
+
+        if (answer.error) {
             throw answer.error
         }
 
+        tweet.tweetInfo = answer
+
         res.status(200).send({
-            tweet: answer,
+            tweet: tweet,
             message: 'tweets and user retrieved successfully'
         })
     }
-    catch{
+    catch {
         res.status(500).send()
     }
 })
