232f9af06737f1b2047d97a1c44879290a3d4556
amrzakii2000 <amrz@silverkeytech.com>
2022-04-24 02:42:40 +0200

    Added cors package

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const config = require('./config');
 
 const adminRoutes = require("./routes/admin");
@@ -13,6 +14,8 @@ const port = 80;
 const connectionurl = config.cloudConnectString;
 
 app.use(express.json());
+app.use(cors());
+
 app.use(adminRoutes);
 app.use(userRoutes);
 app.use(tweetRoutes);
