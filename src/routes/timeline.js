363c3442c9a3d5bf86df6de1f061960ab9c1cf1e
amrzakii2000 <amrz@silverkeytech.com>
2022-04-24 12:05:09 +0200

    Modified pagination

--- src/routes/timeline.js
+++ src/routes/timeline.js
@@ -4,19 +4,19 @@ const userModel = require("../models/user");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/home", auth, async (req, res) => {
+router.get("/home/:page/:count", auth, async (req, res) => {
   try {
     let count = 10;
 
-    if (isNaN(req.body.page) || req.body.page <= 0) {
+    if (isNaN(req.params.page) || req.params.page <= 0) {
       return res.status(400).send({ message: "Invalid page number" });
     }
 
-    if (!isNaN(req.body.count) && req.body.count >= 0) {
-      count = req.body.count;
+    if (!isNaN(req.params.count) && req.params.count >= 0) {
+      count = req.params.count;
     }
 
-    const page = parseInt(req.body.page);
+    const page = parseInt(req.params.page);
     const usersIds = [req.user._id, ...req.user.followings];
 
     const result = await tweetModel
--- src/routes/tweet.js
+++ src/routes/tweet.js
@@ -23,70 +23,80 @@ router.delete("/status/tweet/delete", auth, async (req, res) => {
   }
 });
 
-router.get("/status/tweets/list/:username", auth, async (req, res) => {
-  try {
-    let count = 10;
-    if (isNaN(req.body.page) || req.body.page <= 0) {
-      return res.status(400).send({ message: "Invalid page number" });
-    }
-
-    if (!isNaN(req.body.count) && req.body.count >= 0) {
-      count = req.body.count;
-    }
-
-    const page = parseInt(req.body.page);
-    let tweets = undefined;
-    tweets = await Tweet.find({ username: req.params.username })
-      .sort({
-        createdAt: -1,
-      })
-      .skip(count * (page - 1))
-      .limit(count);
-
-    if (!tweets) {
-      return res.status(404).send({ message: "Invalid username" });
-    }
-
-    const tweetObjects = [];
-    for (let i = 0; i < tweets.length; i++) {
-      const tweetObject = await Tweet.getTweetObject(tweets[i]);
-      if (req.query.include_replies === "true") {
-        const tweetObjectWithReplies = await Tweet.getTweetReplies(tweetObject);
-        tweetObjects.push(tweetObjectWithReplies);
-      } else {
-        tweetObjects.push(tweetObject);
+router.get(
+  "/status/tweets/list/:username/:page/:count",
+  auth,
+  async (req, res) => {
+    try {
+      let count = 10;
+      if (isNaN(req.params.page) || req.params.page <= 0) {
+        return res.status(400).send({ message: "Invalid page number" });
       }
-    }
 
-    res.status(200).send({
-      tweets: tweetObjects,
-    });
-  } catch(error) {
-    res.status(500).send(error.toString());
+      if (!isNaN(req.params.count) && req.params.count >= 0) {
+        count = req.params.count;
+      }
+
+      const page = parseInt(req.params.page);
+      let tweets = undefined;
+      tweets = await Tweet.find({ username: req.params.username })
+        .sort({
+          createdAt: -1,
+        })
+        .skip(count * (page - 1))
+        .limit(count);
+
+      if (!tweets) {
+        return res.status(404).send({ message: "Invalid username" });
+      }
+
+      const tweetObjects = [];
+      for (let i = 0; i < tweets.length; i++) {
+        const tweetObject = await Tweet.getTweetObject(tweets[i], false);
+        if (req.query.include_replies === "true") {
+          const tweetObjectWithReplies = await Tweet.getTweetReplies(
+            tweetObject
+          );
+          tweetObjects.push(tweetObjectWithReplies);
+        } else {
+          tweetObjects.push(tweetObject);
+        }
+      }
+
+      res.status(200).send({
+        tweets: tweetObjects,
+      });
+    } catch (error) {
+      res.status(500).send({ message: "Internal Server Error" });
+    }
   }
-});
+);
 
 router.get("/status/tweet/:id", async (req, res) => {
   try {
     const tweet = await Tweet.findById(req.params.id).populate({
       path: "userId",
-      select: "username name profile_picture -_id",
     });
 
     if (!tweet) {
       return res.status(404).send({ message: "Invalid Tweet Id" });
     }
-    
-    const tweetObject = await Tweet.getTweetObject(tweet);
-    const tweetWithReplies = await Tweet.getTweetReplies(tweetObject);
 
-    res.status(200).send({
-      tweet: tweetWithReplies,
-      message: "Tweet has been retrieved successfully",
-    });
+    const tweetObject = await Tweet.getTweetObject(tweet);
+    if (req.query.include_replies === "true") {
+      const tweetWithReplies = await Tweet.getTweetReplies(tweetObject);
+      res.status(200).send({
+        tweet: tweetWithReplies,
+        message: "Tweet has been retrieved successfully",
+      });
+    } else {
+      res.status(200).send({
+        tweet: tweetObject,
+        message: "Tweet has been retrieved successfully",
+      });
+    }
   } catch (err) {
-    //res.status(500).send({ "message": "Internal Server Error" });
-    res.status(500).send(err.toString());
+    res.status(500).send({ message: "Internal Server Error" });
   }
 });
 
