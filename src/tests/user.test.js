52638a5e6103a3b249a5c469b72d40ceca01ad87
ahmedelgarf94 <ahmedelgarf94@gmail.com>
2022-04-27 06:01:57 +0200

    Update user.test.js

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -209,7 +209,7 @@ test("Should get notifications list", async () => {
         .send({ email_or_username: userOne.email, password: userOne.password })
         .expect(200);
 
-    const user = await getUser(userOne.email);
+    const user = await getUser(userOne.username);
     const response = await request(app)
         .get("/notifications/list")
         .set("Authorization", "Bearer " + user.tokens[0].token)
