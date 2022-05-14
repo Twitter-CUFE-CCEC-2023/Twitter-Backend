3740716ae034bbebb703f3a28422ac1234ae1cb1
amrzakii2000 <amrz@silverkeytech.com>
2022-05-14 14:26:15 +0200

    Update tweet.js

--- src/routes/tweet.js
+++ src/routes/tweet.js
@@ -5,11 +5,41 @@ const Like = require("../models/like");
 const auth = require("../middleware/auth");
 const Notification = require("../models/notification");
 const NotificationType = require("./../../seed-data/constants/notificationType");
-const upload = require('../services/fileUpload');
-require("multer");
-require("path");
-require("./../config");
+const multer = require("multer");
+const path = require("path");
+const config = require("./../config");
 const router = express.Router();
+
+const maxFileSize = 50 * 1024 * 1024;
+
+const storage = multer.diskStorage({
+  destination: config.uploadPath,
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: maxFileSize },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|gif|pdf|docx|doc|ppt|mp4|mpeg/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Unsupported file type");
+  }
+}
 
 router.delete("/status/tweet/delete", auth, async (req, res) => {
   try {
