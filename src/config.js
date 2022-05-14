7c08f96bb46c26623cff41bfee0cbf6ad9b16169
amrzakii2000 <amrz@silverkeytech.com>
2022-05-14 13:48:54 +0200

    Revert "Added configurations for media uploading"
    This reverts commit afbf0678e1d6991d0eed87e1afc09c54f6b74757.

--- src/config.js
+++ src/config.js
@@ -4,5 +4,3 @@ exports.testConnectionString = "mongodb://localhost:27017/twitter-clone-testDB";
 exports.googleClientID =
   "894199507999-vb42l34pcp44b2jddb82cd4a1cjusj1u.apps.googleusercontent.com";
 exports.googleClientSecret = "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";
-exports.baseUrl = "https://backlb.twittercloneteamone.tk";
-exports.uploadPath = "./uploads/";
